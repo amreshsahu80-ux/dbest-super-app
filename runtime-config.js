4afb1391e7fc0f267f1e9af8397471a82fb06873
amreshsahu80-ux <amresh.sahu80@gmail.com>
2026-08-21 08:15:52 +0530

    Add runtime config for Supabase owner auth

---
+++ runtime-config.js
@@ -0,0 +1,6 @@
+window.DBEST_RUNTIME_CONFIG = Object.freeze({
+  ownerEmail: "amresh.sahu80@gmail.com",
+  supabaseUrl: "https://ydedmotbacnllkijzmvp.supabase.co",
+  supabasePublishableKey: "sb_publishable_qWqQExdtL5ddWcK_yLCcNA__-ZUBtox",
+  integrationBranch: "backend-integration"
+});
