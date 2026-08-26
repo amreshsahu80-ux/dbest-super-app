5848a57dd372ee54feee05970bd5fa60f6dada89
amreshsahu80-ux <amresh.sahu80@gmail.com>
2026-08-26 16:38:35 +0530

    Refresh vendor agreement email OTP display fix

--- runtime-config.js
+++ runtime-config.js
@@ -6,7 +6,7 @@ window.DBEST_RUNTIME_CONFIG = Object.freeze({
 });
 
 (function(){
-  const V='20260826-1650-vendor-promo-mount';
+  const V='20260826-1705-vendor-agreement-email';
 
   const installLogoClarity=()=>{
     if(document.getElementById('dbestLogoClarityStyle')) return;
