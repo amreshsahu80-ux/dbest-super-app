1de19b491806efe76f14d76715423b27364af7f1
amreshsahu80-ux <amresh.sahu80@gmail.com>
2026-08-28 13:30:43 +0530

    Refresh Vaahak 90 second offer build

--- runtime-config.js
+++ runtime-config.js
@@ -6,7 +6,7 @@ window.DBEST_RUNTIME_CONFIG = Object.freeze({
 });
 
 (function(){
-  const V='20260828-1335-vaahak-stable-orders';
+  const V='20260828-1345-vaahak-90sec-offer';
 
   const installLogoClarity=()=>{
     if(document.getElementById('dbestLogoClarityStyle')) return;
