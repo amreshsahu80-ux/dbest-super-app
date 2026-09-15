84ad68bbf0c7ec9976ed2563644e8a0c3f8c6775
amreshsahu80-ux <amresh.sahu80@gmail.com>
2026-09-16 01:37:16 +0530

    Load simplified marketplace handover UI across Vendor and Vaahak portals

--- runtime-config.js
+++ runtime-config.js
@@ -7,7 +7,7 @@ window.DBEST_RUNTIME_CONFIG = Object.freeze({
 });
 
 (function(){
-  const V='20260915-vendor-pin-reset-v1';
+  const V='20260916-marketplace-simple-handover-v1';
 
   const applyRuntimeSecrets=()=>{
     const sec=window.DBEST_RUNTIME_SECRETS||{};
@@ -58,6 +58,7 @@ window.DBEST_RUNTIME_CONFIG = Object.freeze({
   if(/\/vendor(?:\.html)?\/?$/i.test(location.pathname)){
     loadScript('/vendor-clean-catalog-tools.js?v='+V,'data-dbest-vendor-clean-catalog-tools');
     loadScript('/vendor-pin-reset-ui-v1.js?v='+V,'data-dbest-vendor-pin-reset-ui-v1');
+    loadScript('/vendor-delivery-pin-ui-v1.js?v='+V,'data-dbest-vendor-handover-ui-v1');
   }
   if(/\/owner-live-onboarding\.html\/?$/i.test(location.pathname))loadScript('/owner-review-files-v1.js?v='+V,'data-dbest-owner-review-files-v1');
 
